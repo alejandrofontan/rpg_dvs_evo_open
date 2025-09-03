1d6196bc655d1e4f0f40a155c6a9815211ad280f
alejandrofontan <fontanvillacampa@gmail.com>
2025-09-03 10:51:40 +1000

    update dependencies

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
-# #!/bin/bash
+#!/bin/bash
 
-echo "First, we install the required apt packages"
-sudo add-apt-repository ppa:inivation-ppa/inivation
-sudo apt-get update
-sudo apt-get install            \
-    ros-$1-sophus               \
-    ros-$1-pcl-ros              \
-    ros-$1-eigen-conversions    \
-    ros-$1-camera-info-manager  \
-    ros-$1-image-view           \
-    libcaer-dev                 \
-    libfftw3-dev libfftw3-doc   \
-    libglew-dev                 \
-    libopencv-dev               \
-    libyaml-cpp-dev             \
-    python-catkin-tools         \
-    ros-$1-camera-info-manager  \
-    ros-$1-image-geometry
+# echo "First, we install the required apt packages"
+# sudo add-apt-repository ppa:inivation-ppa/inivation
+# sudo apt-get update
+# sudo apt-get install            \
+#     ros-$1-sophus               \
+#     ros-$1-pcl-ros              \
+#     ros-$1-eigen-conversions    \
+#     ros-$1-camera-info-manager  \
+#     ros-$1-image-view           \
+#     libcaer-dev                 \
+#     libfftw3-dev libfftw3-doc   \
+#     libglew-dev                 \
+#     libopencv-dev               \
+#     libyaml-cpp-dev             \
+#     python-catkin-tools         \
+#     ros-$1-camera-info-manager  \
+#     ros-$1-image-geometry
 
 
 
